4d52d8f486bb3f5a6f2382f68c262d85b657c2ef
gizat-u <giza_uteev@mail.ru>
2022-06-23 20:05:49 +0500

    v0.4 user-dto

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -1,11 +1,21 @@
 module.exports = class UserDto {
 	id;
+	name;
 	email;
+	phone;
+	role;
+	image;
+	balance;
 	isActivated;
 
 	constructor(model) {
 		this.id = model._id;
+		this.name = model.name;
 		this.email = model.email;
+		this.phone = model.phone;
+		this.role = model.role;
+		this.image = model.image;
+		this.balance = model.balance;
 		this.isActivated = model.isActivated;
 	}
 }
